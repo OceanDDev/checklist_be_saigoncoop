b44e05a72470213b0972b58ba56bb55ca7ab72b1
duong.do <duong.binhdo@hitachids.com>
2025-06-20 20:54:12 +0700

    update cors

--- server.js
+++ server.js
@@ -12,10 +12,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-cors({
-    origin: "https://checklist-fe-saigoncoop.onrender.com", 
-    credentials: true,
-  })
+app.use(cors());
 app.use(express.json());
 
 // Routes
