d77182c681e8b5046ac3ee6fdcfa84aef734b437
duong.do <126135281+OceanDDev@users.noreply.github.com>
2025-07-05 15:48:12 +0700

    update routes

--- routes/checklistform/checklistform.routes.js
+++ routes/checklistform/checklistform.routes.js
@@ -5,12 +5,23 @@ const {
   createChecklistForm,
   getAllChecklistForms,
   getChecklistFormById,
+  updateChecklistForm,
+  deleteChecklistForm,
 } = require("../../controllers/checklistform/checklistform.controller");
 
+// Tạo checklist form
 router.post("/checklistform", createChecklistForm);
+
+// Lấy tất cả checklist form
 router.get("/checklistform", getAllChecklistForms);
+
+// Lấy checklist form theo ID
 router.get("/checklistform/:id", getChecklistFormById);
 
+// Cập nhật checklist form theo ID
+router.put("/checklistform/:id", updateChecklistForm);
 
+// Xóa checklist form theo ID
+router.delete("/checklistform/:id", deleteChecklistForm);
 
 module.exports = router;
